605b14b918da19a00ffc718e2ffc60fd8b39c7ba
Jacob Groß <kurtextrem@gmail.com>
2018-05-11 18:25:06 +0200

    Remove quotes

--- src/name.js
+++ src/name.js
@@ -2,7 +2,7 @@ export const createMeasure = name => (...args) =>
   typeof name == 'function' ? name(...args) : name.toString();
 
 export const getStartMark = (id, measureName, ...args) =>
-  `'\u269B' ${id}:${measureName(...args)}:start`;
+  `\u269B ${id}:${measureName(...args)}:start`;
 
 export const getEndMark = (id, measureName, ...args) =>
-  `'\u269B' ${id}:${measureName(...args)}:end`;
+  `\u269B ${id}:${measureName(...args)}:end`;
